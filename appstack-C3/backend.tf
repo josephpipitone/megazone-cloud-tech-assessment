474bef58319639d69b97ffcb4762ef8279f53568
Joe Pipitone <joseph.pipitone@gmail.com>
2025-11-21 11:41:15 -0500

    feat: encrypt s3 state for appstack

--- appstack-C3/backend.tf
+++ appstack-C3/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket       = "mzc-assessment-prod-terraform-state"
     key          = "application/mzc-app-prod/terraform.tfstate"
     region       = "us-east-1"
+    encrypt      = true
     profile      = "mzc-infra-prod"
     use_lockfile = true
   }
